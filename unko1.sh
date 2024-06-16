745334aa45d6ace971aa969c3a9c4a952932919b
furandolu <166970841+furandolu@users.noreply.github.com>
2024-06-16 21:51:22 +0900

    Update unko1.sh

--- unko1.sh
+++ unko1.sh
@@ -1,1 +1,5 @@
 echo "うんこ"
+echo "おせちんこ"
+zenity --error --title="おいしいたけ" --text="PCやりすぎ" 2>/dev/null
+zenity --warning --title="おせちおせちおせちんこ" --text="おせちおせちおせちんこ" 2>/dev/null
+zenity --info --title="ブログどうぞ" --text="reimu59549.f5.si" 2>/dev/null
